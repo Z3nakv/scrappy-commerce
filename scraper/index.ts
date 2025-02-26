e3e0a106ec613ab4783520194bf6b49078795709
Adrian <ad.rivarola18@gmail.com>
2025-02-26 00:44:57 -0500

    first commit

---
+++ scraper/index.ts
@@ -0,0 +1,23 @@
+import puppeteer from "puppeteer";
+
+export async function scrapeProducts () {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    try {
+        await page.goto('https://portfolio-psi-orcin-14.vercel.app/', {
+        });
+
+        const grabTitle = await page.evaluate(() => {
+            const pgTitle = document.querySelector("p.text-subtitle");
+            return pgTitle ? pgTitle?.innerHTML : 'no se encontro titulo';
+        });
+
+        console.log("Título del producto:", grabTitle);
+    } catch (error) {
+        console.error("Error al hacer scraping:", error);
+    } finally {
+        await page.close();
+        await browser.close();
+    }
+}
